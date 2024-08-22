318c7bf513bd6ef3daa8727503fb999b494a8ed2
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-08-22 14:58:10 +0200

    process_001

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Header, Loader } from "./components";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+const Country = lazy(() => import("./pages/Country"));
+const SearchCountry = lazy(() => import("./pages/SearchCountry"));
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country" element={<SearchCountry />} />
+          <Route path="/country/:countryId" element={<Country />} />
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+};
---
+++ src/components/GoBackBtn/GoBackBtn.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+import styles from "./GoBackBtn.module.css";
+
+export const GoBackBtn = ({ children, path }) => {
+  return (
+    <>
+      <Link className={styles.link} to={path}>
+        {children}
+      </Link>
+    </>
+  );
+};

2ecec81c9226a2cb0e0f8d5d0c7c2b038e204591
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-08-22 15:06:18 +0200

    process_002

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { App } from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
 // import { StrictMode } from "react";
 // import { createRoot } from "react-dom/client";
@@ -18,10 +18,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 // import App from "./components/App/App.jsx";
 // import "./index.css";
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// );
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
